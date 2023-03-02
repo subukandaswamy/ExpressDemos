cdf1424f0bcba7c11ebf0cddccc1261b2c268be8
Subu Kandaswamy <b.kandaswamy@wsu.edu>
2023-03-01 16:17:23 -0800

    Lecture 3/1/2023

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # ExpressDemos
 CPT_S 489 Spring 2023 Express Demos
+
+Lecture 2/27/2023
 - [x] Nodemon setup
 - [x] http server without node
         - [x] http module
@@ -12,3 +14,14 @@ CPT_S 489 Spring 2023 Express Demos
     - [x] Basic routing
     - [ ] serving static content
     - [x] sendFile (using path)
+
+Lecture 3/1/2023
+- [x] Middleware
+- [x] Serving Static Content
+- EJS Templating engine
+    - [x] Setting up EJS and Views
+    - [x] Rendering Templates
+    - [ ] scripts in templates
+        - [ ] processing lists
+        - [ ] conditional statements
+        - [ ] %include to compose templates
--- expressserver.js
+++ expressserver.js
@@ -1,18 +1,34 @@
-const path = require("path")
-
 const express = require("express")
 const app = express()
 
-function handle(req, res) {
-    res.send("Hello Express!")
+function validateUser(req, res, next) {
+    // assume that I am doing some DB stuff
+    // and validation user
+    console.log("user is validated");
+    res.locals.name = "Jeremiah"
+    next()
 }
 
+app.use(validateUser)
 
+// app.get("/", (req, res, next) => {
+//     res.send("Hello! Welcome to the Server Side")
+// })
 
-app.get("/index.html", (req, res) => {
-    res.sendFile(path.join(__dirname,"index.html"))
+app.use(express.static("html"))
+
+app.set("view engine","ejs")
+app.set("view", "assets")
+
+app.get("/", (req, res, next) => {
+    // assume that I pulled the username and balance
+    console.log(res)
+    //res.locals.name = "Logan"
+    res.render("index", {
+        balance: "1000000"
+    })
 })
 
-app.get('*',handle)
-
-app.listen(3000)
+app.listen(3000, () => {
+    console.log("server started in port 3000");
+})
